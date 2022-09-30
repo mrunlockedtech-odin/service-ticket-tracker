177f6358defa8e1fec1de4c522a3ee8e96edb18e
mrunlockedtech <jrbhrn@gmail.com>
2022-09-30 15:13:44 -0500

    Initial Commit

---
+++ middleware/middleware.js
@@ -0,0 +1,16 @@
+function passDataToView(req, res, next) {
+  res.locals.user = req.user ? req.user : null
+  res.locals.googleLoginURL = process.env.GOOGLE_LOGIN_URL
+  res.locals.googleClientID = process.env.GOOGLE_CLIENT_ID
+  next()
+}
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next()
+  res.redirect('/')
+}
+
+export {
+  passDataToView,
+  isLoggedIn,
+}
