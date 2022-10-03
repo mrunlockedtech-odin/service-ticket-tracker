b56a54c621ea0b73991518e43776313c00a195d5
mrunlockedtech <jrbhrn@gmail.com>
2022-10-03 13:03:44 -0500

    Add permissions to viewing tickets on user profiles

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,90 +1,96 @@
 import { Profile } from '../models/profile.js'
 import { Ticket } from '../models/ticket.js'
 
-function index(req,res) {
+function index(req, res) {
   Profile.find({})
-  .then(profiles => {
-    res.render('profiles/index', {
-      title:'Users',
-      profiles:profiles
-    })
-  })
-}
-function adminPass(req,res){
-  Profile.findById(req.user.profile._id)
-  .then(profile => {
-    if(profile.isAdmin){
-      res.redirect('/profiles/adminList')
-    }else {
-      res.render('profiles/admAuth',{
-        title: 'Admin Password Entry',
-        wrongPass: false,
-      })
-    }
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect(`/profiles/${req.user.profile._id}`)
-  })
-
-}
-
-function showAdmins(req,res){
-  if(req.body.password === "CarlWeezer"){
-    Profile.findById(req.user.profile._id)
-    .then(profile => {
-      profile.isAdmin = true
-      profile.save()
-    })
-    res.redirect('/profiles/adminList')
-  }else {
-    res.render('profiles/admAuth',{
-      wrongPass:true,
-      title:"Admin Password Entry"
-    })
-  }
-}
-function showAdminsList(req,res){
-Profile.findById(req.user.profile._id)
-.then(profile => {
-  if(profile.isAdmin){
-    Profile.find({})
     .then(profiles => {
-      res.render('profiles/adminList',{
-        title:'Admins',
-        profiles:profiles
+      res.render('profiles/index', {
+        title: 'Users',
+        profiles: profiles
       })
     })
+}
+function adminPass(req, res) {
+  Profile.findById(req.user.profile._id)
+    .then(profile => {
+      if (profile.isAdmin) {
+        res.redirect('/profiles/adminList')
+      } else {
+        res.render('profiles/admAuth', {
+          title: 'Admin Password Entry',
+          wrongPass: false,
+        })
+      }
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect(`/profiles/${req.user.profile._id}`)
+    })
 
-  } else {
-    res.render('error')
-  }
-})
 }
 
-function adminStatusChange(req,res){
+function showAdmins(req, res) {
+  if (req.body.password === "CarlWeezer") {
+    Profile.findById(req.user.profile._id)
+      .then(profile => {
+        profile.isAdmin = true
+        profile.save()
+      })
+    res.redirect('/profiles/adminList')
+  } else {
+    res.render('profiles/admAuth', {
+      wrongPass: true,
+      title: "Admin Password Entry"
+    })
+  }
+}
+function showAdminsList(req, res) {
+  Profile.findById(req.user.profile._id)
+    .then(profile => {
+      if (profile.isAdmin) {
+        Profile.find({})
+          .then(profiles => {
+            res.render('profiles/adminList', {
+              title: 'Admins',
+              profiles: profiles
+            })
+          })
+
+      } else {
+        res.render('error')
+      }
+    })
+}
+
+function adminStatusChange(req, res) {
   console.log(req.params)
   Profile.findById(req.params.id)
-  .then(profile => {
-    profile.isAdmin = !(profile.isAdmin)
-    profile.save()
-    res.redirect('/profiles/adminList')
-  })
+    .then(profile => {
+      profile.isAdmin = !(profile.isAdmin)
+      profile.save()
+      res.redirect('/profiles/adminList')
+    })
 }
 
-function show(req,res){
+function show(req, res) {
   Profile.findById(req.params.id)
-  .then(profile => {
-    Ticket.find({owner: req.params.id})
-    .then(tickets => {
-      res.render('profiles/show',{
-        profile:profile,
-        title: profile.name,
-        tickets:tickets
-      })
-    })
+    .then(profile => {
+      Ticket.find({ owner: req.params.id })
+        .then(tickets => {
+          Profile.findById(req.user.profile._id)
+            .then(userProfile => {
+              res.render('profiles/show', {
+                profile: profile,
+                title: profile.name,
+                tickets: tickets,
+                userProfile:userProfile
+              })
 
-  })
+            })
+
+        })
+
+    })
 }
 
 export {
